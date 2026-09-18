8cb1078adbbdcdb8056997a4bfc7281399114d40
Loth Limbic <lothar.aguirre@gmail.com>
2026-09-17 22:01:38 -0500

    Refactor professor data and event listeners

--- script.js
+++ script.js
@@ -17,52 +17,62 @@
 const profesores = {
     "Barnett": {
         nombre: "Prof. Oscar A. Barnett C.",
-        asignatura: "Curso: Ofimática 1",
-        aula: "Laboratorio 01 (Piso 04)",
-        horario: "Lunes, De 08:15 am a 10:30 am",
-        delegado: "Juan Romario Campos Machaca",
-        contacto: "https://wa.me/+51900930053",
-        foto: "Imgs/Profs/Barnett.jpg",
-    },
-
-    "Padilla": {
-        nombre: "Ing. Edwin Padilla Obregon",
-        asignatura: "Networking de la Comunicación / Lógica Computacional",
-        aula: "405 - IA - Diurno (Piso 04)",
-        horario: "Lunes, de 09:45 am a 12:15 pm.<br>Miércoles, de 08:15 am a 09:45 am",
-        delegado: "Juan Romario Campos Machaca",
-        contacto: "https://wa.me/+51900930053",
-        foto: "https://via.placeholder.com/150"
+        asignatura: "Curso: Ofimática 1.",
+        aula: "Laboratorio 01 (Piso 04).",
+        horario: "📆 Lunes, de 08:15 am a 10:30 am (Lab. 01).",
+        delegado: "",
+        contacto: "https://wa.me/+51xxxxxxxxx",
+        foto: "Logo_ISTPA_001.png",
     },
 
     "Gutierrez": {
         nombre: "Prof. Gutierrez",
-        asignatura: "Herramientas para análisis de datos.",
-        aula: "405 - IIA - Diurno (Piso 04)",
-        horario: "Lunes, De 10:30 am a 13:00 pm",
-        delegado: "Jair Moreno P.",
-        contacto: "https://wa.me/+51923563342",
-        foto: "https://via.placeholder.com/150"
+        asignatura: "Herramientas para Análisis de Datos.",
+        aula: "405 - IIA - Diurno (Piso 04).",
+        horario: "📆 Lunes, De 10:30 am a 13:00 pm.",
+        delegado: "",
+        contacto: "https://wa.me/+51xxxxxxxxx",
+        foto: "Logo_ISTPA_001.png",
+    },
+
+    "Huertas": {
+        nombre: "Prof. Gina Huertas C.",
+        asignatura: "Curso: Fundamentos De Interfaces Web Interactivas.",
+        aula: "405 - IIA - Diurno & Lab. 01 (Piso 04).",
+        horario: "Martes, de 09.00 am a 11:15 am.",
+        delegado: "",
+        contacto: "https://wa.me/+51xxxxxxxxx",
+        foto: "Logo_ISTPA_001.png",
+    },
+
+    "De La Cruz": {
+        nombre: "Prof. Pedro De La Cruz",
+        asignatura: "Curso: Lenguaje De Programación 1.",
+        aula: "405 - IIA - Diurno & Lab. 01 (Piso 04).",
+        horario: "<p>📆 Martes, de 11:30 am a 13:45 pm (Lab. 01).</p><p>📆 Miércoles, de 08:15 am a 09:45 am (Aula 405).</p><p></p>",
+        delegado: "",
+        contacto: "https://wa.me/+51xxxxxxxxx",
+        foto: "Logo_ISTPA_001.png",
+    },
+
+    "Padilla": {
+        nombre: "Prof. Edwin Padilla Obregon",
+        asignatura: "Cursos: Reparación De Equipos Tecnológicos & Diseño y Configuración De Redes.",
+        aula: "405 - IIA - Diurno (Piso 04).",
+        horario: "<p>📆 Miércoles, de 10:30 am a 13:00 pm.</p><p>📆 Jueves, de 08:15 am a 10:30 am.</p><p>📆 Viernes, de 08:15 am a 09:45 am.</p><p></p>",
+        delegado: "",
+        contacto: "https://wa.me/+51xxxxxxxxx",
+        foto: "Logo_ISTPA_001.png",
     },
 
      "Palomino": {
         nombre: "Prof. Palomino",
-        asignatura: "Programación de Macrtos",
-        aula: "405 - IIA - Lab. 01 (Piso 04)",
-        horario:  "Jueves, de 08:15 am a 09:45 am // Viernes, de 10:30 am a 13:45 pm",
-        delegado: "Juan Romario Campos Machaca",
-        contacto: "https://wa.me/+51900930053",
-        foto: "https://via.placeholder.com/150"
-    },
-    
-"Galindo": {
-        nombre: "Ing. Juan Luis Galindo Rojas",
-        asignatura: "Fundamentos De Interfaces Web Interactivas",
-        aula: "405 - IA - Diurno (Piso 04)",
-        horario: "08:15 a 10:30 h",
-        delegado: "Juan Romario Campos Machaca",
-        contacto: "https://wa.me/+51900930053",
-        foto: "https://via.placeholder.com/150"
+        asignatura: "Curso: Programación de Macros.",
+        aula: "405 - IIA - Diurno & Lab. 01 (Piso 04).",
+        horario: "<p>📆 Jueves, de 08:15 am am a 09:45 am.</p><p>📆 Viernes, de 08:15 am a 10:30 am.</p><p></p>",
+        delegado: "",
+        contacto: "https://wa.me/+51xxxxxxxxx",
+        foto: "Logo_ISTPA_001.png",
     },
 
     "Castillo": {
@@ -70,9 +80,9 @@ const profesores = {
         asignatura: "Comunicación Oral",
         aula: "405 - IIA - Diurno (Piso 04)",
         horario: "Jueves, de 11:30 am  a 13:45 pm",
-        delegado: "Juan Romario Campos Machaca",
-        contacto: "https://wa.me/+51900930053",
-        foto: "https://via.placeholder.com/150"
+        delegado: "",
+        contacto: "https://wa.me/+51xxxxxxxxx",
+        foto: "Logo_ISTPA_001.png",
     },
 
     "Libre": {
@@ -82,7 +92,7 @@ const profesores = {
         horario: "",
         delegado: "",
         contacto: "https://wa.me/+51900930053",
-        foto: "https://via.placeholder.com/150"
+        foto: "Logo_ISTPA_001.png",
     },
 
     "Salida": {
@@ -90,9 +100,9 @@ const profesores = {
         asignatura: "Sales temprano hoy. 😄",
         aula: "",
         horario: "",
-        delegado: "Juan Romario Campos Machaca",
-        contacto: "https://wa.me/+51900930053",
-        foto: "https://via.placeholder.com/150"
+        delegado: "",
+        contacto: "https://wa.me/+51xxxxxxxxx",
+        foto: "Logo_ISTPA_001.png",
     },
 
     // Agrega más profesores aquí...
@@ -107,14 +117,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
             const texto = celda.textContent.trim();
-            let profesorKey = "Barnett"; // Por defecto
+            let profesorKey = "Libre"; // Por defecto
 
-            if (texto.includes("Padilla")) profesorKey = "Padilla";
+            if (texto.includes("Barnett")) profesorKey = "Barnett";
             if (texto.includes("Gutierrez")) profesorKey = "Gutierrez";
+            if (texto.includes("Huertas")) profesorKey = "Huertas";
+            if (texto.includes("De La Cruz")) profesorKey = "De La Cruz";
+            if (texto.includes("Padilla")) profesorKey = "Padilla";
             if (texto.includes("Palomino")) profesorKey = "Palomino";
-            if (texto.includes("Galindo")) profesorKey = "Galindo";
             if (texto.includes("Castillo")) profesorKey = "Castillo";
-            if (texto.includes("LIBRE")) profesorKey = "Libre";
             if (texto.includes("SALIDA")) profesorKey = "Salida";
 
             // Agrega más condiciones según necesites
